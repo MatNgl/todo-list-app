754b7670dbc3182bc519b05ad5d266914ad9b227
MatNgl <mattheo.naegellen@gmail.com>
2025-09-02 21:39:21 +0200

    fin partie 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent], // Supprimez TodoListComponent
+  template: `
+    <app-header></app-header>
+    <main class="container mx-auto p-4">
+      <router-outlet></router-outlet> <!-- Utilisez router-outlet -->
+    </main>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'todo-list-app';
+}
